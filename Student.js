f6747f7d7aa36e81eeb9b098e5828fd8db50a3eb
Aesha1-Patel <102579722+Aesha1-Patel@users.noreply.github.com>
2022-04-28 13:07:33 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(name, age, year){
+       this.name=name;
+       this.age=age;
+       this.year=year;
+   }
+display(){
+    console.log(this.name)
+    console.log(this.age)
+    console.log(this.year)
+}
+}
+
+
+
---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+  createCanvas(400,400);
+student1= new Student("Anna", 14, 10);
+student1.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
